57f65aa709523b9508c237e76ccf845b2695a2fe
Binupchacko13 <binupchacko@zohomail.in>
2025-10-26 12:52:37 +0530

    Assignment1 completed

---
+++ Assign1Task1.py
@@ -0,0 +1,7 @@
+# Basic arithmetc operations
+a=int(input("Enter the first number  : "))
+b=int(input("Enter the second number : "))
+print("Addition of two numbers       : ", a+b)
+print("Subtraction of two numbers    : ", a-b)
+print("Multiplication of two numbers : ", a*b)
+print("Division of two numbers       : ", a/b)
